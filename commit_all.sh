0ff8fcbf35ade49fa9fbee61fd37d6d1633b2759
laromicas <laromicas@hotmail.com>
2024-06-14 22:37:10 -0500

    Update datoso version

---
+++ commit_all.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/zsh
+
+REPOS=(datoso datoso_plugin_internetarchive datoso_seed_base datoso_seed_fbneo datoso_seed_md_enhanced datoso_seed_nointro datoso_seed_pleasuredome datoso_seed_private datoso_seed_redump datoso_seed_sfc_enhancedcolors datoso_seed_sfc_msu1 datoso_seed_sfc_speedhacks datoso_seed_tdc datoso_seed_translatedenglish datoso_seed_vpinmame datoso_seed_whdload)
+
+
+for repo in $REPOS; do
+    echo "Commiting $repo"
+    cd $repo
+    git add .
+    git commit -m "Update $repo version"
+    git push
+    cd ..
+done
