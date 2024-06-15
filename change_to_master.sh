286960092d5558d792dc2026d4c052a98dcb1369
laromicas <laromicas@hotmail.com>
2024-06-14 23:02:44 -0500

    Update datoso version

--- change_to_master.sh
+++ change_to_master.sh
@@ -2,11 +2,12 @@
 
 REPOS=(datoso datoso_plugin_internetarchive datoso_seed_base datoso_seed_fbneo datoso_seed_md_enhanced datoso_seed_nointro datoso_seed_pleasuredome datoso_seed_private datoso_seed_redump datoso_seed_sfc_enhancedcolors datoso_seed_sfc_msu1 datoso_seed_sfc_speedhacks datoso_seed_tdc datoso_seed_translatedenglish datoso_seed_vpinmame datoso_seed_whdload)
 
-cd ..
+
 for repo in $REPOS; do
-    echo "Change $repo branch to master"
+    echo "Commiting $repo"
     cd $repo
-    git checkout master
-    git pull
+    git add .
+    git commit -m "Update $repo version"
+    git push
     cd ..
 done
