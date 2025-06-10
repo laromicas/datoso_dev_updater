e7e88eda237f0cace25002d45b52f2ba6f0eb26d
laromicas <laromicas@hotmail.com>
2025-06-10 08:03:15 -0500

    Changing base

--- plugins.py
+++ plugins.py
@@ -1,21 +1,17 @@
 """Plugin version checker."""
+from lib.config import PATH
 from packaging.version import Version
-
-from config import PATH
 
 plugin_list = [
     'datoso',
     'datoso_plugin_internetarchive',
     'datoso_seed_base',
+    'datoso_seed_enhanced',
     'datoso_seed_fbneo',
-    'datoso_seed_md_enhanced',
     'datoso_seed_nointro',
     'datoso_seed_pleasuredome',
     'datoso_seed_private',
     'datoso_seed_redump',
-    'datoso_seed_sfc_enhancedcolors',
-    'datoso_seed_sfc_msu1',
-    'datoso_seed_sfc_speedhacks',
     'datoso_seed_tdc',
     'datoso_seed_translatedenglish',
     'datoso_seed_vpinmame',
